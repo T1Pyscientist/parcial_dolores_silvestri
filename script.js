96ec04e6d04273e38540e532f9aca70b78279dcd
Juan Silvestri <silvestrijuanignacio@gmail.com>
2023-04-11 18:17:09 -0300

    plots

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-d3.dsv(":", "enero.csv", d3.autoType).then(data => {
+d3.dsv(";", "enero.csv", d3.autoType).then(data => {
 
-    console.log(data)
+    data = data.filter(d =>  d.categoria !== null)
 
     // Categorias por canal
     let plot1 = Plot.plot({
@@ -8,12 +8,58 @@ d3.dsv(":", "enero.csv", d3.autoType).then(data => {
             Plot.barY(data, 
                 Plot.groupX(
                     {y: 'count'}, 
-                    {x: 'canal', y: 'count', fill: 'canal'}
+                    {x: 'canal', y: 'count', fill: 'categoria'}
                 ),
             )
         ],
 
+        y: {domain: [0, 10000]},
+        color: {legend: true},
+
     })
 
-    d3.select('#chart').append(() => plot1)
+    d3.select('#chart1').append(() => plot1)
+});
+
+d3.dsv(";", "junio.csv", d3.autoType).then(data => {
+
+    data = data.filter(d =>  d.categoria !== null)
+
+    // Categorias por canal
+    let plot1 = Plot.plot({
+        marks: [
+            Plot.barY(data, 
+                Plot.groupX(
+                    {y: 'count'}, 
+                    {x: 'canal', y: 'count', fill: 'categoria'}
+                ),
+            )
+        ],
+        y: {domain: [0, 10000]}
+
+    })
+
+    d3.select('#chart2').append(() => plot1)
+});
+
+d3.dsv(";", "agosto.csv", d3.autoType).then(data => {
+
+    data = data.filter(d =>  d.categoria !== null)
+
+    // Categorias por canal
+    let plot1 = Plot.plot({
+        marks: [
+            Plot.barY(data, 
+                Plot.groupX(
+                    {y: 'count'}, 
+                    {x: 'canal', y: 'count', fill: 'categoria'}
+                ),
+            )
+        ],
+        y: {domain: [0, 10000]}
+
+    })
+
+    d3.select('#chart3').append(() => plot1)
+
 });
