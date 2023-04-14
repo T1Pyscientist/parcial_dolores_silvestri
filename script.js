e4feff66a92fee20ec0b5bc5defa11a0a321181d
vidaldolores <vidaldolores02@gmail.com>
2023-04-14 19:47:09 -0300

    plantilla html y css

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 d3.dsv(";", "enero.csv", d3.autoType).then(data => {
 
-    data = data.filter(d =>  d.categoria !== null)
+    data = data.filter(d => d.categoria !== null)
 
     // Categorias por canal
     let plot1 = Plot.plot({
         marks: [
-            Plot.barY(data, 
-                Plot.groupX(
-                    {y: 'count'}, 
-                    {x: 'canal', y: 'count', fill: 'categoria'}
+            Plot.barY(data,
+                Plot.groupX({ y: 'count' }, { x: 'canal', y: 'count', fill: 'categoria' },
+
                 ),
             )
         ],
 
-        y: {domain: [0, 10000]},
-        color: {legend: true},
+        y: { domain: [0, 10000] },
+        color: { legend: true },
 
     })
 
@@ -23,19 +22,16 @@ d3.dsv(";", "enero.csv", d3.autoType).then(data => {
 
 d3.dsv(";", "junio.csv", d3.autoType).then(data => {
 
-    data = data.filter(d =>  d.categoria !== null)
+    data = data.filter(d => d.categoria !== null)
 
     // Categorias por canal
     let plot1 = Plot.plot({
         marks: [
-            Plot.barY(data, 
-                Plot.groupX(
-                    {y: 'count'}, 
-                    {x: 'canal', y: 'count', fill: 'categoria'}
-                ),
+            Plot.barY(data,
+                Plot.groupX({ y: 'count' }, { x: 'canal', y: 'count', fill: 'categoria' }),
             )
         ],
-        y: {domain: [0, 10000]}
+        y: { domain: [0, 10000] }
 
     })
 
@@ -44,19 +40,16 @@ d3.dsv(";", "junio.csv", d3.autoType).then(data => {
 
 d3.dsv(";", "agosto.csv", d3.autoType).then(data => {
 
-    data = data.filter(d =>  d.categoria !== null)
+    data = data.filter(d => d.categoria !== null)
 
     // Categorias por canal
     let plot1 = Plot.plot({
         marks: [
-            Plot.barY(data, 
-                Plot.groupX(
-                    {y: 'count'}, 
-                    {x: 'canal', y: 'count', fill: 'categoria'}
-                ),
+            Plot.barY(data,
+                Plot.groupX({ y: 'count' }, { x: 'canal', y: 'count', fill: 'categoria' }),
             )
         ],
-        y: {domain: [0, 10000]}
+        y: { domain: [0, 10000] }
 
     })
 
