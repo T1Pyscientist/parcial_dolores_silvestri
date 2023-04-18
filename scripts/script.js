b355efde6a016845e57d421e106304a3284e8f7a
Juan Silvestri <silvestrijuanignacio@gmail.com>
2023-04-18 20:41:54 -0300

    datos

--- scripts/script.js
+++ scripts/script.js
@@ -42,108 +42,47 @@ d3.dsv(",", "data/denuncias_por_mes.csv", d3.autoType).then(data => {
 
     })
 
-    d3.select('#chart2').append(() => plot2)
     d3.select('#chart1').append(() => plot1)
+    d3.select('#chart2').append(() => plot2)
+
 });
 
 
 const mapaFetch = d3.json('data/barrios-caba.geojson')
-const dataFetch = d3.dsv(';', 'data/enero.csv', d3.autoType)
+const dataFetch = d3.dsv(',', 'data/denuncias_por_barrio1.csv', d3.autoType)
 
 Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
-  
-    data = data.map(d => {
-        if (d.canal === "App Denuncia Vial"){
-            d.canal = "App BA 147"
-        }
-        if (d.canal !== "App BA 147" && d.canal !== "GCS Web") {
-            d.canal = "Otros"
-        } 
-        return d;
-    });
 
-    data = data.filter(d =>  d.canal === "App BA 147")
+    let chartMap = Plot.plot({
 
-    const reclamosPorBarrio = d3.group(data, d => d.domicilio_barrio) // crea un Map
-    console.log(reclamosPorBarrio)
+        projection: {
+            type: 'mercator',
+            domain: barrios, 
+        },
+        color: {
+            type: 'quantize', 
+            n:5,
+            scheme: 'ylorbr',
+            label: 'Cantidad de denuncias',
+            legend: true,
+        },
+        marks: [
+            Plot.geo(barrios, {
+                fill: d => {
+                    let nombreBarrio = d.properties.BARRIO
+                    let denunciasBarrio = data.filter(d => d.Barrio == nombreBarrio)[0]
+                    let cantReclamos = denunciasBarrio.Agosto - denunciasBarrio.Julio 
+                    return cantReclamos
+                },
+                stroke: '#777777',
+            }),
+        ],
+        width: 350
+        
+    })    
 
-  let chartMap = Plot.plot({
-    // https://github.com/observablehq/plot#projection-options
-    projection: {
-      type: 'mercator',
-      domain: barrios, // Objeto GeoJson a encuadrar
-    },
-    color: {
-        // Quantize continuo (cant. denuncias) -> discreto (cant. colores)
-        type: 'quantize', 
-        n: 10,
-        scheme: 'ylorbr',
-        label: 'Cantidad de denuncias',
-        legend: true,
-      },
-    marks: [
-      Plot.geo(barrios, {
-        fill: d => {
-            let nombreBarrio = d.properties.BARRIO
-            let cantReclamos = reclamosPorBarrio.get(nombreBarrio).length
-            return cantReclamos
-          },
-        stroke: '#777777',
-        title: d => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
-      }),
-    ],
-    
-  })
 
-  /* Agregamos al DOM la visualización chartMap */
-  d3.select('#mapa').append(() => chartMap)
-})
+    /* Agregamos al DOM la visualización chartMap */
+    d3.select('#mapa1').append(() => chartMap)
 
-Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
-  
-    data = data.map(d => {
-        if (d.canal === "App Denuncia Vial"){
-            d.canal = "App BA 147"
-        }
-        if (d.canal !== "App BA 147" && d.canal !== "GCS Web") {
-            d.canal = "Otros"
-        } 
-        return d;
-    });
-
-    data = data.filter(d =>  d.canal === "GCS Web")
-
-    const reclamosPorBarrio = d3.group(data, d => d.domicilio_barrio) // crea un Map
-    console.log(reclamosPorBarrio)
-
-  let chartMap = Plot.plot({
-    // https://github.com/observablehq/plot#projection-options
-    projection: {
-      type: 'mercator',
-      domain: barrios, // Objeto GeoJson a encuadrar
-    },
-    color: {
-        // Quantize continuo (cant. denuncias) -> discreto (cant. colores)
-        type: 'quantize', 
-        n: 10,
-        scheme: 'ylorbr',
-        label: 'Cantidad de denuncias',
-        legend: true,
-      },
-    marks: [
-      Plot.geo(barrios, {
-        fill: d => {
-            let nombreBarrio = d.properties.BARRIO
-            let cantReclamos = reclamosPorBarrio.get(nombreBarrio).length
-            return cantReclamos
-          },
-        stroke: '#777777',
-        title: d => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
-      }),
-    ],
-    
-  })
-
-  /* Agregamos al DOM la visualización chartMap */
-  d3.select('#mapa').append(() => chartMap)
 })
